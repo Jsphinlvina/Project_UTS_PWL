b5bff941495c4d150f33801ea675ddc5affed23b
Josephine Alvina L <josephinealvinaluwia@gmail.com>
2024-04-10 20:09:56 +0700

    Ubah Design Navbar dan Sidebar
    Ada css tambahan sama js juga

--- main/resources/views/dashboard/sidebar.blade.php
+++ main/resources/views/dashboard/sidebar.blade.php
@@ -1,69 +1,43 @@
-<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu"
-         aria-labelledby="sidebarMenuLabel">
+<div class="sidebarcol-md-3 col-lg-2 p-0 nav-background" id="sidemenu">
+    <div class="offcanvas-md offcanvas-end " tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
         <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+            <h5 class="offcanvas-title" id="sidebarMenuLabel">Universitas Kristen Maranatha</h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu"
                     aria-label="Close"></button>
         </div>
-        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+        <div class="offcanvas-body nav-background d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2 text-dark {{Request::is('/') ? 'active': ''}}"
+                    <a class="nav-link d-flex align-items-center gap-2 {{Request::is('/') ? 'active': ''}}"
                        aria-current="page" href="/">
-                        <svg class="bi">
-                            <span class="bi bi-house"></span>
-                        </svg>
-                        Home
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2 text-dark
-                    {{Request::is('dashboard/polling')
-                        ? 'active': ''}}" href="/dashboard/polling">
-                        <svg class="bi">
-                            <i class="bi bi-file-post"></i>
-                        </svg>
-                        Polling Mata Kuliah
+                        <i class="bi bi-house-fill pb-4"></i>
+                        Dashboard
                     </a>
                 </li>
                 @can('kaprodi')
                     <li class="nav-item">
-                        <a class="nav-link d-flex align-items-center gap-2 text-dark {{Request::is('dashboard/matakuliah') ? 'active': ''}}"
+                        <a class="nav-link d-flex align-items-center gap-2 {{Request::is('dashboard/users') ? 'active': ''}}"
+                           href="/dashboard/users">
+                            <i class="bi bi-people pb-4"></i>
+                            User
+                        </a>
+                    </li>
+                @endcan
+                @can('kaprodi')
+                    <li class="nav-item">
+                        <a class="nav-link d-flex align-items-center gap-2 {{Request::is('dashboard/matakuliah') ? 'active': ''}}"
                            href="/dashboard/mata-kuliah">
-                            <svg class="bi">
-                                <i class="bi bi-file-post"></i>
-                            </svg>
+                            <i class="bi bi-list-task pb-4"></i>
                             Mata Kuliah
                         </a>
                     </li>
                 @endcan
-                @can('kaprodi')
-                    <li class="nav-item">
-                        <a class="nav-link d-flex align-items-center gap-2 text-dark {{Request::is('dashboard/users') ? 'active': ''}}"
-                           href="/dashboard/users">
-                            <svg class="bi">
-                                <i class="bi bi-file-post"></i>
-                            </svg>
-                            Users
-                        </a>
-                    </li>
-                @endcan
-            </ul>
-            <hr class="my-3">
-            <ul class="nav flex-column mb-auto">
                 <li class="nav-item">
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <a class="nav-link d-flex align-items-center gap-2 text-dark" href="route('logout')"
-                           onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                            <svg class="bi">
-                                <i class="bi bi-door-closed"></i>
-                            </svg>
-                            {{ __('Log Out') }}
-                        </a>
-                    </form>
+                    <a class="nav-link d-flex align-items-center gap-2 {{Request::is('dashboard/polling')? 'active': ''}}"
+                       href="/dashboard/polling">
+                        <i class="bi bi-file-post pb-4"></i>
+                        Polling Mata Kuliah
+                    </a>
                 </li>
             </ul>
         </div>
