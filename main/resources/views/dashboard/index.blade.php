43d70fe85bca315d06949a7a48acfc8a1bf42bab
Elmosius Suli <2272008@maranatha.ac.id>
2024-04-10 20:25:05 +0700

    Add files via upload

--- main/resources/views/dashboard/index.blade.php
+++ main/resources/views/dashboard/index.blade.php
@@ -13,43 +13,31 @@
             </div>
         @endif
 
-        <div class="table-responsive small col-lg-6
-        ">
+        <div class="table-responsive small col-lg-6">
             <table class="table table-striped table-sm mt-2">
                 <thead>
                 <tr>
-                    <th scope="col">Kode Mata Kuliah</th>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Jumlah polling</th>
+                    <th scope="col">Nomor Polling</th>
+                    <th scope="col">Periode</th>
+                    <th scope="col">Nama Pengguna</th>
+                    <th scope="col">Jumlah SKS</th>
                 </tr>
                 </thead>
                 <tbody>
-{{--                @foreach($posts as $post)--}}
-{{--                    <tr>--}}
-{{--                        <td>{{$loop->iteration }}</td>--}}
-{{--                        <td>{{$post->title}}</td>--}}
-{{--                        <td>{{$post->category->name}}</td>--}}
-{{--                        <td>--}}
-{{--                            <a href="/dashboard/posts/{{$post->id}}" class="badge bg-success">--}}
-{{--                                <span class="bi bi-eye"></span>--}}
-{{--                            </a>--}}
-{{--                            <a href="/dashboard/posts/{{$post->id}}/edit" class="badge bg-warning">--}}
-{{--                                <span class="bi bi-pencil-square"></span>--}}
-{{--                            </a>--}}
-{{--                            <form method="post" action="/dashboard/posts/{{$post->id}}" class="d-inline">--}}
-{{--                                @method('delete')--}}
-{{--                                @csrf--}}
-{{--                                <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')">--}}
-{{--                                    <span class="bi bi-x"></span>--}}
-{{--                                </button>--}}
-{{--                            </form>--}}
-{{--                        </td>--}}
-{{--                    </tr>--}}
-{{--                @endforeach--}}
+                @foreach($datas as $pol)
+                    <tr>
+                        <td>{{$pol->polling->id_polling}}</td>
+                        <td>
+                            {{ \Carbon\Carbon::parse($pol->polling->start_at)->format('Y-m-d') }}
+                            - {{ \Carbon\Carbon::parse($pol->polling->end_at)->format('Y-m-d') }}
+                        </td>
+                        <td>{{$pol->users->nama_user}}</td>
+{{--                        <td>{{$pol->where('id_mataKuliah', $pol->mataKuliah->id_mataKuliah)->sum('sks')}}</td>--}}
+                    </tr>
+                @endforeach
                 </tbody>
             </table>
         </div>
         <canvas class="my-4 w-100" id="myChart" width="900" height="500"></canvas>
     </main>
 @endsection
-
